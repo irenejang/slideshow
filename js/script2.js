7db5479c321a50af8aff45350720125a127b76c7
irenejang <irenejang@gmail.com>
2015-10-19 17:56:38 -0500

    002

--- js/script2.js
+++ js/script2.js
@@ -1,19 +1,60 @@
-$('.img-container img:gt(0)').hide();
+var btnIndex;
+var activeIndex;
+
+function fadeOut() {
+	$('.active-img').fadeOut();
+}
+
+
+$('.img-container li:gt(0)').hide();
 
 $('.buttons li').click(function() {
 	$(this).addClass('active-button').siblings().removeClass('active-button');
 });
 
 $('.buttons li').click(function() {
-	var btnIndex = $(this).index(); // store button index
+	fadeOut();
+	var btnIndex = $(this).index(); 
+	$('.photos').eq(btnIndex).fadeIn();
 	$('.photos').eq(btnIndex).addClass('active-img').siblings().removeClass('active-img');
-})
-
-// $('photos').index().
-
-$('.buttons li').eq(1).click(function() {
-	$('.photos').eq(0).fadeOut().eq(1).fadeIn();
 });
+
+$('.prev').click(function() {
+	var imgIndex = $('.active-img').index();
+	fadeOut();
+	if (imgIndex > 0) {
+		$('.photos').eq(imgIndex - 1).fadeIn();
+		$('.photos').eq(imgIndex - 1).addClass('active-img').siblings().removeClass('active-img');
+	} else {
+		$('.photos').eq($('.photos').length - 1).fadeIn();
+		$('.photos').eq($('.photos').length - 1).addClass('active-img').siblings().removeClass('active-img');
+	}
+	var activeIndex = $('.active-img').index();
+	$('buttons li').eq(activeIndex).addClass('active-button').siblings().removeClass('active-button');
+});
+
+$('.next').click(function() {
+	var imgIndex = $('.active-img').index();
+	fadeOut();
+	if (imgIndex < 4) {
+		$('.photos').eq(imgIndex + 1).fadeIn();
+		$('.photos').eq(imgIndex + 1).addClass('active-img').siblings().removeClass('active-img');
+	} else {
+		$('.photos').eq(0).fadeIn();
+		$('.photos').eq(0).addClass('active-img').siblings().removeClass('active-img');
+	}
+	var activeIndex = $('.active-img').index();
+	$('buttons li').eq(activeIndex).addClass('active-button').siblings().removeClass('active-button');
+});
+
+
+
+
+
+// $('.buttons li').eq(1).click(function() {
+// 	$('.photos').eq(0).fadeOut();
+// 	$('.photos').eq(1).fadeIn();
+// });
 
 
 
